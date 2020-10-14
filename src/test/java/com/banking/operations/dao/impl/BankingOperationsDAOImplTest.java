f611a9637cdab84d76aea424061b6fe7140e2913
Jeffrytom <jeffryabraham2014@gmail.com>
2020-10-14 13:46:26 +0530

    banking operations

--- src/test/java/com/banking/operations/dao/impl/BankingOperationsDAOImplTest.java
+++ src/test/java/com/banking/operations/dao/impl/BankingOperationsDAOImplTest.java
@@ -30,29 +30,9 @@ public class BankingOperationsDAOImplTest {
         bankingOperationsDAOImplUnderTest.mongoTemplate = mock(MongoTemplate.class);
     }
 
-    //@Test
-    public void testFindUserByUserName() {
-        // Setup
-
-        // Configure MongoTemplate.find(...).
-        Criteria criteria = mock(Criteria.class);
-        Query query = mock(Query.class);
-
-        final UserDetailsEntityDTO userDetailsEntityDTO = new UserDetailsEntityDTO();
-        final List<UserDetailsEntityDTO> userDetailsEntityDTOS = Arrays.asList(userDetailsEntityDTO);
-
-        //when(query.addCriteria(Criteria.where(anyString()).is("userName"))).thenReturn(new Query());
-        when(bankingOperationsDAOImplUnderTest.mongoTemplate.find(new Query(any()), UserDetailsEntityDTO.class)).thenReturn(userDetailsEntityDTOS);
-
-        // Run the test
-        final UserDetailsEntityDTO result = bankingOperationsDAOImplUnderTest.findUserByUserName("userName");
-
-        // Verify the results
-    }
 
     @Test
     public void testUpdateAccountDetails() {
-        // Setup
         final UserDetailsEntityDTO userDetails = new UserDetailsEntityDTO();
         userDetails.setAddress1("temp1");
         userDetails.setAddress2("temp2");
@@ -64,14 +44,11 @@ public class BankingOperationsDAOImplTest {
         accounts.setBalance(500);
         userDetails.setAccounts(Arrays.asList(accounts));
 
-        // Configure MongoTemplate.save(...).
         final UserDetailsEntityDTO userDetailsEntityDTO = new UserDetailsEntityDTO();
         when(bankingOperationsDAOImplUnderTest.mongoTemplate.save(any(UserDetailsEntityDTO.class))).thenReturn(userDetailsEntityDTO);
 
-        // Run the test
         final UserDetailsEntityDTO result = bankingOperationsDAOImplUnderTest.updateAccountDetails(userDetails);
 
-        // Verify the results
 
         Assert.assertNotNull(result);
         Assert.assertEquals(userDetails.getAddress1(), "temp1");
